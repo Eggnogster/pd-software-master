543a413e22a4c57d4e8bde09a68529559a247712
Eggnogster <erikwantland@gmail.com>
2016-08-27 19:52:59 -0500

    Updated Settings again

--- privateduty/wsgi.py
+++ privateduty/wsgi.py
@@ -26,3 +26,9 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
