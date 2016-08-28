c289f6e99ac02a64cfa484bf499823890880e3ce
Eggnogster <erikwantland@gmail.com>
2016-08-27 19:13:40 -0500

    Changed folder locations

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pd_software.wsgi
---
+++ virtual/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\erikw\Documents\GitHub\pd-software-master\privateduty\virtual\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
