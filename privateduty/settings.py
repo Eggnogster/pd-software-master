d5f13d3a72ce7425de68603ee10fa90c31f2cc94
Eggnogster <erikwantland@gmail.com>
2016-08-27 20:14:16 -0500

    Updated Settings

--- privateduty/settings.py
+++ privateduty/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
 
@@ -119,5 +120,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- privateduty/wsgi.py
+++ privateduty/wsgi.py
@@ -26,9 +26,5 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
-
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
